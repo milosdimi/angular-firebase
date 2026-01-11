8818e2c3d221c8bbce6e79ff82a0523366d3f0a4
milosdimi <132084913+milosdimi@users.noreply.github.com>
2026-01-11 19:25:28 +0100

    firebase instaled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { NoteListComponent } from './note-list/note-list.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -10,7 +9,7 @@ import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.compon
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NoteListComponent, FooterComponent, HeaderComponent, AddNoteDialogComponent],
+  imports: [CommonModule, NoteListComponent, FooterComponent, HeaderComponent, AddNoteDialogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,23 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyAPj_RWpCtIUnle2i5H8oNpeY4bZi-ZWgE',
+        authDomain: 'danotes-3f8ef.firebaseapp.com',
+        projectId: 'danotes-3f8ef',
+        storageBucket: 'danotes-3f8ef.firebasestorage.app',
+        messagingSenderId: '457026107942',
+        appId: '1:457026107942:web:8ab3ca9f6028f44fb484e7',
+        // measurementId: 'G-XXXXXXX' // nur wenn Analytics benutzt wird
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
