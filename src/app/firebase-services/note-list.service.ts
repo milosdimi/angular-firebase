799010cc3a3c0afd54d8513c5033f15b02152269
milosdimi <132084913+milosdimi@users.noreply.github.com>
2026-01-11 23:05:53 +0100

    firebase works

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { collection, Firestore, doc, collectionData } from '@angular/fire/firestore';
+import { Note } from '../interfaces/note.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
 
-  constructor() { }
+  items$;
+  firestore: Firestore = inject(Firestore);
+  constructor() {
+    this.items$ = collectionData(this.getNotesRef());
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
