d4d28f1267c614352d0d3b4aa06b4007e310994c
milosdimi <132084913+milosdimi@users.noreply.github.com>
2026-01-12 01:06:56 +0100

    firebase onSnapshot works

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,18 +1,59 @@
-import { inject, Injectable } from '@angular/core';
-import { collection, Firestore, doc, collectionData } from '@angular/fire/firestore';
+import { inject, Injectable, OnDestroy } from '@angular/core';
+import {
+  collection,
+  Firestore,
+  doc,
+  onSnapshot,
+} from '@angular/fire/firestore';
 import { Note } from '../interfaces/note.interface';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NoteListService {
+export class NoteListService implements OnDestroy {
   trashNotes: Note[] = [];
   normalNotes: Note[] = [];
 
-  items$;
+  unsubTrash;
+  unsubNotes;
+
   firestore: Firestore = inject(Firestore);
+
   constructor() {
-    this.items$ = collectionData(this.getNotesRef());
+    this.unsubNotes = this.subNotesList();
+    this.unsubTrash = this.subTrashList();
+  }
+
+  ngOnDestroy() {
+    this.unsubNotes();
+    this.unsubTrash();
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach((element) => {
+        this.trashNotes.push(this.setNoteObjects(element.data(), element.id));
+      });
+    });
+  }
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach((element) => {
+        this.normalNotes.push(this.setNoteObjects(element.data(), element.id));
+      });
+    });
+  }
+
+  setNoteObjects(obj: any, id: string): Note {
+    return {
+      id: id,
+      type: obj.type || 'note',
+      title: obj.title || '',
+      content: obj.content || '',
+      marked: obj.marked || false,
+    };
   }
 
   getNotesRef() {
